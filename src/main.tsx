96db373016f2cee26b2eb1220ba66e85495b1e9c
DocFishy <165524714+DocFishy@users.noreply.github.com>
2025-09-18 22:03:52 +0300

    Deploy site

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+const root = document.getElementById("root") as HTMLElement;
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(root).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
